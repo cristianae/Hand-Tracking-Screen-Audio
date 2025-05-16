7fb30b5b4736f0ff8e0fc121bf6c494a6bd5b78d
cristianae <Cristiana.M.Eagen-1@ou.edu>
2025-05-15 21:20:20 -0500

    added a quit button

--- handtracking.py
+++ handtracking.py
@@ -4,6 +4,7 @@ import mediapipe
 handSolution = mediapipe.solutions.hands #importing mediapipe library
 hand = handSolution.Hands() #creating an object of hands class
 videoCap= cv2.VideoCapture(0) #0 is the default camera
+quitapplication = False
 
 if not videoCap.isOpened():
     print("Camera is not opening. ")
@@ -11,23 +12,35 @@ if not videoCap.isOpened():
 
 print("Camera opened successfully")
 
-if cv2.waitKey(1) & 0xFF == ord('q'):
-    print("Exiting the application.")
-    exit()
+def mouseClick(event, x, y, flags, param):
+    global quitapplication
+    if event == cv2.EVENT_LBUTTONDOWN: #if left mouse button is clicked
+        if 10 <= x <= 110 and 10 <= y <= 50: 
+            quitapplication = True #set quitapplication to true
 
+cv2.namedWindow("My Camera") 
+cv2.setMouseCallback("My Camera", mouseClick) #set mouse callback function
 
 while True:
     success, img= videoCap.read() #reading image, which returns an array of pixel values 
-    if success:
-        movement = hand.process(img) #process the image
-        if movement.multi_hand_landmarks: #if hand is detected
-            for handLandmarks in movement.multi_hand_landmarks: #for each hand detected
-                for id, lm in enumerate(handLandmarks.landmark): #enumerate gives index and value
-                    h, w, c = img.shape #height, width, channels of image
-                    cx, cy = int(lm.x * w), int(lm.y * h) #x and y coordinates of hand landmarks
-                    cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED) #draw circle on hand landmarks
-        cv2.imshow("CamOutput", img) #showing image on separate window
-        cv2.waitKey(1) #wait for 1ms
+    if not success:
+        break
+    movement = hand.process(img) #process the image
+    if movement.multi_hand_landmarks: #if hand is detected
+        for handLandmarks in movement.multi_hand_landmarks: #for each hand detected
+            for id, lm in enumerate(handLandmarks.landmark): #enumerate gives index and value
+                h, w, c = img.shape #height, width, channels of image
+                cx, cy = int(lm.x * w), int(lm.y * h) #x and y coordinates of hand landmarks
+                cv2.circle(img, (cx, cy), 5, (255, 0, 255), cv2.FILLED) #draw circle on hand landmarks
+        
+    cv2.rectangle(img, (10, 10), (110, 50), (0, 0, 255), -1)
+    cv2.putText(img, 'QUIT', (25, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
 
+    cv2.imshow("My Camera", img) #showing the camera feed
+    cv2.waitKey(1)
+    if quitapplication: #wait for 1ms
+        break
+       
 
- 
+videoCap.release()
+cv2.destroyAllWindows()
