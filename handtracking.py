84baccfa62fb4bc54ff1653298e3940b838bf290
cristianae <Cristiana.M.Eagen-1@ou.edu>
2025-05-15 23:36:54 -0500

    brightness of the screen is working

--- handtracking.py
+++ handtracking.py
@@ -10,6 +10,7 @@ videoCap= cv2.VideoCapture(0) #0 is the default camera, opens the webcam
 quitapplication = False
 min = 30
 max = 200
+brightness = 1.0
 
 if not videoCap.isOpened():
     print("Camera is not opening. ")
@@ -26,14 +27,19 @@ def mouseClick(event, x, y, flags, param):
 cv2.namedWindow("My Camera") 
 cv2.setMouseCallback("My Camera", mouseClick) #set mouse callback function
 
+#note: ulti_hand_landmarks is for hand landmarks while multi_handedness is for multiple hands
 
 while True:
     success, img= videoCap.read() #reading image, which returns an array of pixel values 
     if not success:
         break
     movement = hand.process(img) #process the image
-    if movement.multi_hand_landmarks: #if hand is detected
-        for handLandmarks in movement.multi_hand_landmarks: #for each hand detected
+    
+
+    if movement.multi_hand_landmarks and movement.multi_handedness: 
+        for i, handLandmarks in enumerate(movement.multi_hand_landmarks):
+            label = movement.multi_handedness[i].classification[0].label  #"Left" or "Right"
+
             for id, lm in enumerate(handLandmarks.landmark):
                 h, w, c = img.shape #height, width, channels of image
                 cx, cy = int(lm.x * w), int(lm.y * h)
@@ -52,10 +58,23 @@ while True:
             
             min = np.minimum(distance + 50, min)
             max = np.maximum(distance, max)
-            volume = np.clip((distance - min) / (max - min) * 100, 0, 100)
-             #change volume on mac os
-            call(["osascript", "-e", "set volume output volume {}".format(int(volume))])    
-            cv2.putText(img, f'Volume: {int(volume)}%', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+
+            if label == "Left": #if left hand is detected
+                volume = np.clip((distance - min) / (max - min) * 100, 0, 100)
+                #change volume on mac os
+                call(["osascript", "-e", "set volume output volume {}".format(int(volume))])    
+                cv2.putText(img, f'Volume: {int(volume)}%', (10, 100), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 255, 0), 2)
+            
+            elif label == "Right": #if right hand is detected
+                brightness = np.clip((distance - min) / (max - min), 0, 1.0)
+                copydisplay = img.copy()
+                alpha= 1.0 - brightness
+
+                cv2.rectangle(copydisplay, (0, 0), (img.shape[1], img.shape[0]), (0, 0, 0), -1)
+                cv2.addWeighted(copydisplay, alpha, img, 1 - alpha, 0, img)
+                cv2.putText(img, f'Brightness: {int(brightness * 100)}%', (10, 140), cv2.FONT_HERSHEY_SIMPLEX, 1, (255, 255, 0), 2)
+
+
         
     cv2.rectangle(img, (10, 10), (110, 50), (0, 0, 255), -1)
     cv2.putText(img, 'QUIT', (25, 40), cv2.FONT_HERSHEY_SIMPLEX, 0.8, (255, 255, 255), 2)
